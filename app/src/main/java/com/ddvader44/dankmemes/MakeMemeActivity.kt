a0ae7775672536f16e4976de8c5d69c9ba447a49
Dishit <40226856+ddvader44@users.noreply.github.com>
2020-10-24 22:57:21 +0530

    make meme layout

---
+++ app/src/main/java/com/ddvader44/dankmemes/MakeMemeActivity.kt
@@ -0,0 +1,11 @@
+package com.ddvader44.dankmemes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MakeMemeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_meme)
+    }
+}
